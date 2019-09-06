74a6c2c7f5126e57db709ca1e84ad77d787d4090
Dmitry Bobylev <dmitryalexeevi4@yandex.ru>
2019-09-06 16:21:18 +0300

    Task #7

--- force-app/main/lightning-components-lesson/aura/StudyParticipantsPanel/StudyParticipantsPanelController.js
+++ force-app/main/lightning-components-lesson/aura/StudyParticipantsPanel/StudyParticipantsPanelController.js
@@ -4,16 +4,35 @@
 
 ({
   doInit: function(component, event, helper) {
-    component.set('v.filter', {
-      status: '',
-      studyTitleTerm: ''
+    let action = component.get('c.getInitData');
+    action.setCallback(this,function(response) {
+      if(response.getState() === 'SUCCESS') {
+        let initData = response.getReturnValue();
+        component.set('v.pagination', initData.pagination);
+        component.set('v.filter', initData.filter);
+        component.set('v.pageRows', initData.pageRows);
+        component.set('v.initialized', true);
+      }
     });
-    component.set('v.pagination', {
-      currentPage: 1,
-      pagesCount: 10
-    })
+    $A.enqueueAction(action);
   },
-  updateTable: function(component, event, helper) {
-    alert('Update table action');
+
+  doSearch: function(component, event, helper) {
+    let action = component.get('c.search');
+    let filter = JSON.stringify(component.get('v.filter'));
+    let pagination = JSON.stringify(component.get('v.pagination'));
+    action.setParams({
+      filter: filter,
+      pagination: pagination
+    });
+    action.setCallback(this, function(response) {
+      debugger;
+      if(response.getState() === 'SUCCESS') {
+        let searchResponse = response.getReturnValue();
+        component.set('v.pagination', searchResponse.pagination);
+        component.set('v.pageRows', searchResponse.pageRows);
+      }
+    });
+    $A.enqueueAction(action);
   }
 });
