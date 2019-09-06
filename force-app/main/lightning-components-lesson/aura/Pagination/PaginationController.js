705f155774435b7bfc4dc18c0ab0fae1a39857d1
Dmitry Bobylev <dmitryalexeevi4@yandex.ru>
2019-09-06 11:39:16 +0300

    Task #6.7

--- force-app/main/lightning-components-lesson/aura/Pagination/PaginationController.js
+++ force-app/main/lightning-components-lesson/aura/Pagination/PaginationController.js
@@ -2,4 +2,18 @@
  * Created by user on 06.09.2019.
  */
 
-({});
+({
+  doPrevious: function(component, even, helper) {
+    var pagination = component.get('v.pagination')
+    if(pagination.currentPage > 1) pagination.currentPage -= 1;
+    component.set('v.pagination', pagination);
+    component.getEvent('onchange').fire();
+  },
+
+  doNext: function(component, even, helper){
+    var pagination = component.get('v.pagination')
+    if(pagination.currentPage < pagination.pagesCount) pagination.currentPage += 1;
+    component.set('v.pagination', pagination);
+    component.getEvent('onchange').fire();
+  }
+});
