52783c1af37dedeb32a1c1bb82f52aac0fccbcba
Dmitry Bobylev <dmitryalexeevi4@yandex.ru>
2019-09-06 11:15:36 +0300

    Task #6.5

--- force-app/main/lightning-components-lesson/aura/StudyParticipantFilter/StudyParticipantFilterController.js
+++ force-app/main/lightning-components-lesson/aura/StudyParticipantFilter/StudyParticipantFilterController.js
@@ -2,4 +2,8 @@
  * Created by user on 05.09.2019.
  */
 
-({});
+({
+  processChanges: function(component, event, helper) {
+    component.getEvent('onchange').fire();
+  }
+});
--- force-app/main/lightning-components-lesson/aura/StudyParticipantsPanel/StudyParticipantsPanelController.js
+++ force-app/main/lightning-components-lesson/aura/StudyParticipantsPanel/StudyParticipantsPanelController.js
@@ -2,4 +2,18 @@
  * Created by user on 05.09.2019.
  */
 
-({});
+({
+  doInit: function(component, event, helper) {
+    component.set('v.filter', {
+      status: '',
+      studyTitleTerm: ''
+    });
+    component.set('v.pagination', {
+      currentPage: 1,
+      pagesCount: 10
+    })
+  },
+  updateTable: function(component, event, helper) {
+    alert('Update table action');
+  }
+});
